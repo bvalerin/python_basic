180ca4cd38eaac9ded89b655e9de63e1ded08399
Bryan Valerin <bryanvalerin@gmail.com>
2023-06-30 19:15:39 -0600

    Control

---
+++ control.py
@@ -0,0 +1,37 @@
+# Estructuras de Control
+# Se usan para tomar decisiones
+
+# Plan ir a Heredia
+
+#Desicion 1: 
+    # En que va?
+    # Tiene vehiculo?
+    # Cuanto tiene que echar de combustible?
+    # Que el total disponible alcanze para echarle el total que necesita?
+    # Cual ruta es mas rapida?
+
+user = new User
+vehicle = "Honda"
+
+if(user.availableVechicles > 0):
+    if(vehicle.fuel < 50):
+        echar ...
+
+    if(vehicle.fuel < 50):
+        echar ...
+
+    if(vehicle.fuel < 50):
+        echar ...
+
+    elif(vehicle.tires.inflada > 90):
+
+    elif(vehicle.tires.inflada > 90):
+
+    else: 
+        escoger ruta
+
+
+
+
+
+     
