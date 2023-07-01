eb22d5596fc12b0fe9f960f642828a3341d66386
Bryan Valerin <bryanvalerin@gmail.com>
2023-06-30 19:41:08 -0600

    Loops

---
+++ loops.py
@@ -0,0 +1,22 @@
+
+#Iterar es ejecutar varias veces una accion
+list = ["Bryan", "Steven", "Trey"]
+
+contador = 1
+for name in list:
+        for elemento in range(1, 1000000):
+            print("Iteracion numero " + str(elemento) + " de : " + name)
+
+counter = 1
+while(counter < 100):
+      print("Ejecucion numero: " + str(counter))
+      counter += 1
+
+
+
+for letra in "Steven":
+    print(letra)
+
+
+
+
